286e00b5328935098c5a0857e2e7ed69d3d1b016
shaojm <sdfjklmin@gmail.com>
2021-12-20 09:38:10 +0800

    ttt

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20211218.1742';
+const cacheName = 'chirpy-20211220.0934';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
