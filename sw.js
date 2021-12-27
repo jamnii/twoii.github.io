7e8b046a3b65e2523d9c1ff48e119c351d48de82
shaojm <sdfjklmin@gmail.com>
2021-12-27 14:14:38 +0800

    Update some blog

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20211223.1422';
+const cacheName = 'chirpy-20211227.1412';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
